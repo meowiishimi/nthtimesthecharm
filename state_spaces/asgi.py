c6916278f0bee3007145d4616c1ca94e973f9e5d
Keithlog <161996366+Keithlog@users.noreply.github.com>
2025-11-15 22:10:30 +0800

    created the agents app and the state_spaces project

--- state_spaces/asgi.py
+++ state_spaces/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for nthproj project.
+ASGI config for state_spaces project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nthproj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'state_spaces.settings')
 
 application = get_asgi_application()
